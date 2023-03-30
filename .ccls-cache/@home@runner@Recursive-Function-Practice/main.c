e28b83c3d0d8f0959c80c2d3ed91abeaf44c5968
cutedazai <62661342+CuteDazai@users.noreply.github.com>
2023-03-30 18:45:31 +0000

    // 백준 25314번 반복문 연습

--- .ccls-cache/@home@runner@Recursive-Function-Practice/main.c
+++ .ccls-cache/@home@runner@Recursive-Function-Practice/main.c
@@ -1,15 +1,11 @@
-// 백준 25304번 반복문 연습
+// 백준 25314번 반복문 연습
 #include <stdio.h>
 
 int main(void) {
-  int total, type, price, num, sum=0;
-  scanf("%d %d", &total, &type);
-  for(int i=0; i<type; ++i)
-    {
-      scanf("%d %d", &price, &num);
-      sum += price*num;
-    }
-  if(total==sum) printf("Yes");
-  else printf("No");
+  int byte_num=0;
+  scanf("%d", &byte_num);
+  for(int i=0; i<byte_num/4; ++i)
+    printf("long ");
+  printf("int");
 }
 
